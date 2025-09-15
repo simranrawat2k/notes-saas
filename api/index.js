efec68e9ac9346dd7c53302be79f49386bcc0d76
simranrawat <simranrawat2k@gmail.com>
2025-09-15 20:04:13 +0530

    warning fixed

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
 const serverless = require('serverless-http');
 const app = require('../backend/src/app');
 
-module.exports.handler = serverless(app);
+module.exports = async (req, res) => {
+  // Wrap the Express app with serverless-http
+  const handler = serverless(app);
+  return handler(req, res);
+};
