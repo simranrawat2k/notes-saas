5a29b567b03e5660edf463cc7c987f707937abd8
simranrawat <simranrawat2k@gmail.com>
2025-09-15 19:46:20 +0530

    warning fixed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,22 +1,9 @@
 // src/config/db.js
 const mongoose = require('mongoose');
 
-let isConnected = false; // cache the connection
-
 async function connectDB(uri) {
   if (!uri) throw new Error('MONGODB_URI is required');
-
-  if (isConnected) {
-    console.log('MongoDB already connected');
-    return;
-  }
-
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  isConnected = true;
+  await mongoose.connect(uri);
   console.log('MongoDB connected');
 }
 
